5a0ab28022780a0934c6aee50cd724f35ab7e3da
Liza Star <lizadstar18@gmail.com>
2020-12-05 13:49:39 -0600

    products printing

--- chemiclean.ui/chemiclean/src/components/pages/AllProducts/AllProducts.js
+++ chemiclean.ui/chemiclean/src/components/pages/AllProducts/AllProducts.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import './AllProducts.scss';
+import ProductData from '../../../helpers/data/ProductData';
+import ProductCard from '../../shared/ProductCard/ProductCard';
 
 class AllProducts extends React.Component {
+  state = {
+    products: [],
+  }
+
+  getProducts = () => {
+    ProductData.getAllProducts()
+      .then((response) => { this.setState({ products: response }); })
+      .catch((err) => console.error('cannot get products', err));
+  }
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
   render() {
+    const { products } = this.state;
+    const buildProductPage = products.map((product, index) => (
+      <ProductCard key={index} product={product} />
+    ));
     return (
-      <div>
-        Products!
+      <div className="Products text-center">
+        <h2> Products </h2>
+        <div className="ProductContainer d-flex flex-wrap justify-content-around">
+          {buildProductPage}
+        </div>
       </div>
     );
   }
