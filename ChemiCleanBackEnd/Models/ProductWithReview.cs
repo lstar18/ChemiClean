00a20c6d653d4c8f0c06ef7be533ece0c65a6e4b
Liza Star <lizadstar18@gmail.com>
2020-12-07 22:11:29 -0600

    wip

---
+++ ChemiCleanBackEnd/Models/ProductWithReview.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChemiCleanBackEnd.Models
+{
+    public class ProductWithReview
+    {
+        public int ProductId { get; set; }
+        public string uid { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string imageUrl { get; set; }
+        public string rating { get; set; }
+        public int reviewId { get; set; }
+        public int ReviewId { get; set; }
+        public string review { get; set; }
+        public string reviewTitle { get; set; }
+        public DateTime datePosted { get; set; }
+    }
+}
--- ChemiCleanBackEnd/Models/Review.cs
+++ ChemiCleanBackEnd/Models/Review.cs
@@ -7,5 +7,12 @@ namespace ChemiCleanBackEnd.Models
 {
     public class Review
     {
+        public int ReviewId { get; set; }
+        public string Uid { get; set; }
+        public string review { get; set; }
+        public string reviewTitle { get; set; }
+        public DateTime datePosted { get; set; }
+        public int ProductId { get; set; }
+        
     }
 }
