a339f3766da5c5658eaf09dc456ce87f10430689
Liza Star <lizadstar18@gmail.com>
2020-12-16 21:53:09 -0600

    styled reviews

--- chemiclean.ui/src/components/shared/ReviewCard/ReviewCard.js
+++ chemiclean.ui/src/components/shared/ReviewCard/ReviewCard.js
@@ -13,15 +13,15 @@ class ReviewCard extends React.Component {
     const { review, removeReview, Uid } = this.props;
     const UserDeleteReview = () => {
       if (review.uid === Uid) {
-        return <button className="delete-review-button btn btn-outline-danger mt-3 ml-4" onClick={() => removeReview(review.reviewId)}> <i className="fas fa-trash"> </i>  </button>;
+        return <button className="delete-review-button btn btn-outline-danger col-4 mb-3" onClick={() => removeReview(review.reviewId)}> <i className="fas fa-trash"> </i>  </button>;
       }
       return <></>;
     };
     return (
-      <div className="card border-dark mb-3">
-      <div className="card-header"> <h5 className="text-center" >{review.reviewTitle} </h5> </div>
-      <h5 className="card-title text-center"> {review.review} </h5>
-      <p className="card-text text-center"> {moment(review.datePosted).format('LL')} </p>
+      < div className="reviewCard col-9 offset-3 mb-5">
+        <h5 className="text-center" >{review.reviewTitle} </h5>
+        <p className="card-title text-center"> "{review.review}"" </p>
+        <p className="card-text text-center"> {moment(review.datePosted).format('LL')} </p>
       {UserDeleteReview()}
       </div>
     );
